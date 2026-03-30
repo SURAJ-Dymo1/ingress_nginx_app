bcb7002d6ebb6856b15b77502223b92b8b368cee
suraj <suraj.mane@sciverse.co.in>
2026-03-30 21:13:00 +0530

    Test

--- App_One/vite.config.js
+++ App_One/vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'   // 🔥 THIS IS MISSING
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: "/apple/",
   plugins: [react()],
-  base: '/apple/',  // This MUST match your Ingress path
 })
--- App_Two/vite.config.js
+++ App_Two/vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'   // 🔥 THIS IS MISSING
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: "/banana/",
   plugins: [react()],
-  base: '/banana/',  // This MUST match your Ingress path
 })
